70506d14ace9742d6b92a5ba32f4f1058b8e3e6b
kingzcheung <kingzcheung@gmail.com>
2024-06-26 17:04:22 +0800

    refactor(main): replace purifier.parse with purifier.find
    Replace the usage of purifier.parse with purifier.find to extract
    JSON data from a string. This change improves the string parsing
    logic to handle JSON embedded within other text.

--- demo/main.py
+++ demo/main.py
@@ -1,7 +1,7 @@
 
-
 import purifier
 
 
 if __name__ == '__main__':
-    purifier.parse("{\"name\":\"purifier\",\"age\":\"18\"}")
+    res = purifier.find("Ugatudo \{name\} 710 Cuka Parkway {\"name\":\"purifier\",\"age\":\"18\"}|}{}")
+    print(res) #
